81d200b1b987869bddc679ac570b1c1fb918db31
Lior Heber <lior.heber@kenshoo.com>
2017-12-01 08:11:07 +0200

    Create table wrapper component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React, { PureComponent } from "react";
 import ReactDOM from "react-dom";
 
+import Table from "./components/table";
+
 class App extends PureComponent {
   render() {
-    return (
-      <div>Hello world</div>
-    );
+    return <Table />;
   }
 }
 
